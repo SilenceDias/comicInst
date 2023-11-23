703c316568b893ecfe2abd6b3cdea6610254bb5b
SilenceDias <11dias02@mail.ru>
2023-11-23 16:55:37 +0600

    Working on frontend

---
+++ README.md
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+# comicInst
+Instagram type app for comics fandome, made with SpringBoot and Angular
+=======
+# marvelInst
+Instagram type app for Marvel/Dc fandom. Wrote in Java and Angular.js
+>>>>>>> 444aed69 (working on frontend)
---
+++ client/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from "./material-module";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {interceptorProviders} from "./helper/interceptor.service";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [interceptorProviders],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
